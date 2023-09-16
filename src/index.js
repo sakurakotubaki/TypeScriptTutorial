2da5d0e17ba613500d6ddcf60d02d7b538d75d42
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-09-16 18:27:42 +0900

    add values

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const array = [1, 'Taro', 2, 'Satou', 3, 'Hanako']
+console.log(array)
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+let array:Array<string|number> = [1, 'Taro', 2, 'Satou', 3, 'Hanako']
+console.log(array)
+let hoge: string = "ffff";
